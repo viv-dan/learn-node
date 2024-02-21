a03bdaba5bb2f0f1d6d821716e4ea6794fa925ff
viv-dan <81843793+viv-dan@users.noreply.github.com>
2024-02-21 16:28:30 -0500

    class activity 022124

--- basics/quiz/helloserver.js
+++ basics/quiz/helloserver.js
@@ -1,27 +1,24 @@
 #! /opt/homebrew/bin/node
 const http = require('http')
-const args = process.argv;
-const getConfig = require('../config');
+const args = process.argv
+const getConfig = require('../config')
 
-
-const config = getConfig(args);
-const hostname = config.hostname;
-const port = config.port;
+const config = getConfig(args)
+const hostname = config.hostname
+const port = config.port
 
 const server = http.createServer((req, res) => {
-    if(req.url && req.url.endsWith('/home')) {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Welcome to home!');
-    }
-    else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Invalid request due to bad URL');
-    }
-    
+  if (req.url && req.url.endsWith('/home')) {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Welcome to home!')
+  } else {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({ hostname: hostname, port: port }))
+  }
 })
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`)
 })
--- operations/syncdelete.js
+++ operations/syncdelete.js
@@ -1,7 +1,6 @@
-const fs = require("fs");
-fs.readFile("./operations/data.txt", (err, data) => {
-  if (err) console.log(err.message);
-  else console.log(data);
-});
-fs.unlinkSync("./operations/data.txt");		// delete the file
-
+const fs = require('fs')
+fs.readFile('./operations/data.txt', (err, data) => {
+  if (err) console.log(err.message)
+  else console.log(data)
+})
+fs.unlinkSync('./operations/data.txt') // delete the file
