7b83c1097eac5c4de9e13543de6e1197a9e8b8fa
viv-dan <81843793+viv-dan@users.noreply.github.com>
2024-02-26 16:27:35 -0500

    class activity 022624

--- eventloop/immediate.js
+++ eventloop/immediate.js
@@ -1,23 +1,21 @@
-const fs = require('fs');
+const fs = require('fs')
 
-fs.readFile('./data/test.txt', function(err, data) {
-  const startCallback = Date.now();
+fs.readFile('./data/test.txt', function (err, data) {
+  const startCallback = Date.now()
   // do something that will take 10ms...
   while (Date.now() - startCallback < 10) {
     // do nothing
   }
-  if (err) console.log('Error');
-  else console.log(data);
+  if (err) console.log('Error')
+  else console.log(data)
 
-  const timeoutScheduled = Date.now();
+  const timeoutScheduled = Date.now()
   setTimeout(() => {
-    const delay = Date.now() - timeoutScheduled;
+    const delay = Date.now() - timeoutScheduled
 
-    console.log(`${delay}ms have passed since I was scheduled`);
-  }, 5);
-
-  setImmediate(() => {
-    console.log('I was scheduled to run immediately');
-  });
-});
-
+    console.log(`${delay}ms have passed since I was scheduled`)
+  }, 0)
+})
+setImmediate(() => {
+  console.log('I was scheduled to run immediately')
+})
